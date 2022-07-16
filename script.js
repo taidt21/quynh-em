56f2cc177fa512576770f05aab3c6105c9be55bd
taidt21 <dinhtuantaiok@gmail.com>
2022-07-17 01:46:23 +0700

    quynh

---
+++ script.js
@@ -0,0 +1,14 @@
+function myFunction(){
+    document.getElementById("menu").style.transform="translateX(-500px)";
+  }
+          function myFunction2(){
+               document.getElementById("menu").style.transform="translateX(0px)";
+      }
+      function myFunction3(){
+          document.getElementById("overlay").style.display="block";
+      }
+      function myFunction4(){
+          document.getElementById("overlay").style.display="none";
+          
+      }
+      
