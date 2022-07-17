f45ffaaf18c63771902c725222d9c03b32adec3b
taidt21 <dinhtuantaiok@gmail.com>
2022-07-17 10:01:07 +0700

    ok

--- script.js
+++ script.js
@@ -11,4 +11,13 @@ function myFunction(){
           document.getElementById("overlay").style.display="none";
           
       }
+
+      window.addEventListener("scroll", function() {
+        const header = document.getElementById("header"),
+              scroll = window.pageYOffset | document.body.scrollTop;
+      
+        if (scroll > 1) { header.className = "header fixed-top scrolled"; }
+        else if (scroll <= 1) { header.className = "header fixed-top"; }
+      });
+      
       
